9b3ba9316ddac8f9047aa4b27645de2bfd61dd28
CaptainAG <85942725+CaptainAG@users.noreply.github.com>
2022-04-12 18:22:43 +0200

    Test migration2

--- migrations/Version20220324115945.php
+++ migrations/Version20220324115945.php
@@ -21,15 +21,15 @@ final class Version20220324115945 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         //$this->addSql('ALTER TABLE user ADD hotel_id INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D6493243BB18 FOREIGN KEY (hotel_id) REFERENCES hotel (id)');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D6493243BB18 ON user (hotel_id)');
+        // $this->addSql('ALTER TABLE user ADD CONSTRAINT FK_8D93D6493243BB18 FOREIGN KEY (hotel_id) REFERENCES hotel (id)');
+        // $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D6493243BB18 ON user (hotel_id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE user DROP FOREIGN KEY FK_8D93D6493243BB18');
-        $this->addSql('DROP INDEX UNIQ_8D93D6493243BB18 ON user');
-        $this->addSql('ALTER TABLE user DROP hotel_id');
+        // $this->addSql('ALTER TABLE user DROP FOREIGN KEY FK_8D93D6493243BB18');
+        // $this->addSql('DROP INDEX UNIQ_8D93D6493243BB18 ON user');
+        // $this->addSql('ALTER TABLE user DROP hotel_id');
     }
 }
